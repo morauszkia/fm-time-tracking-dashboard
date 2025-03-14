2a8a39a360b93244f3d910d5edbd40790fa11874
mantis <morauszki.andras@gmail.com>
2025-03-14 19:53:46 +0100

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fm-time-tracking-dashboard/",
   plugins: [react(), tailwindcss()],
 });
